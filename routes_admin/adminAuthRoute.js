d9aa2243f67634d8833cdd961b47289df9705065
polya2z <polya2zofficial@gmail.com>
2023-05-12 17:39:26 +0530

    fff

--- routes_admin/adminAuthRoute.js
+++ routes_admin/adminAuthRoute.js
@@ -32,8 +32,9 @@ router.post("/login",limiter, async (req, res) => {
       });
       return res
         .cookie("admin_auth_token", token, {
-          httpOnly: true,
-          secure: true, 
+          // httpOnly: true,
+          // sameSite: "strict", 
+          // secure: true, 
         })
         .json({
           success: true,
